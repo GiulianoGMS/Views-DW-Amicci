30b2e502cfb9edca4bc505345fc935b46836d755
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2022-09-08 12:00:49 -0300

    Create Extracao_Vdas_Cons.sql

---
+++ Extracao_Vdas_Cons.sql
@@ -0,0 +1,18 @@
+SELECT TO_CHAR(FV.DTAOPERACAO, 'MM') MES,
+             --FV.SEQPRODUTO VENDA_SKU,
+             FV.NROEMPRESA VENDA_NRO_EMPRESA,
+             SUM(FV.QTDOPERACAO) VENDA_QTD,
+             SUM(FV.VVLROPERACAOBRUTO) VENDA_BRUTA,
+             SUM(FV.VLROPERACAO)               VENDA_LIQUIDA,
+             SUM(FV.VVLRCTOBRUTO)     VENDA_CUSTO_BRUTO,
+             SUM(FV.VVLRCTOLIQUIDO)   VENDA_CUSTO_LIQUIDO,
+             --fv.seqnf VENDA_CUPOM,
+             --sum(fv.vlrverbavda) AS VENDA_BONIFICACAO,
+             --0 VENDA_CONTRATO,
+             ROUND(SUM( FV.vlroperacao - FV.vvlrctoliquido - FV.vvlrimpostosaida - nvl( FV.vlrdespoperacionalitem, 0 ) - FV.vlrcomissao- FV.vlrverbacompra - FV.vlrcalcimpostovda ),2) VENDA_VLR_MARGEM,
+             ROUND((SUM( FV.vlroperacao - FV.vvlrctoliquido - FV.vvlrimpostosaida - nvl( FV.vlrdespoperacionalitem, 0 ) - FV.vlrcomissao- FV.vlrverbacompra - FV.vlrcalcimpostovda ) / SUM(FV.VLROPERACAO)*100),2) "VENDA_MARGEM"
+
+FROM FATO_VENDA FV
+WHERE FV.CODGERALOPER IN (37,48,123,610,615,613,810,916,910,911)
+AND FV.DTAOPERACAO BETWEEN '01-JUN-2022' AND '31-AUG-2022'
+GROUP BY TO_CHAR(FV.DTAOPERACAO, 'MM'), FV.NROEMPRESA
